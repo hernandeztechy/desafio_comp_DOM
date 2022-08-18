3dae9e332de5cb47f840df68aac4ab40f3756491
Techy <techy05.92@gmail.com>
2022-08-18 17:06:28 -0600

    objeto tareas

---
+++ app.js
@@ -0,0 +1,9 @@
+//tareas por realizar
+const ToDoList = 
+[
+    {tarea:"Compras de supermercado", categoria:"Hogar", hecho:false},
+    {tarea:"Tarea JS DOM", categoria:"Educacion", hecho:true},
+    {tarea:"Sacar al perro", categoria:"Hogar", hecho:false},
+    {tarea:"Enviar correo a Juan", categoria:"Trabajo", hecho:false}
+]
+
