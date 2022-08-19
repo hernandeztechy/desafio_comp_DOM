24e76308a038f90c2ce31a86d78982c698221d25
Techy <techy05.92@gmail.com>
2022-08-19 12:19:57 -0600

    Submit Form

--- app.js
+++ app.js
@@ -7,3 +7,23 @@ const ToDoList =
     {tarea:"Enviar correo a Juan", categoria:"Trabajo", hecho:false}
 ]
 
+const frmTarea = document.getElementById("frmTarea")
+
+frmTarea.onsubmit = datosForm
+
+function datosForm(e)
+{
+    e.preventDefault(); //para evitar que el formulario vaya en blanco
+
+    let datos = e.target //capturar los datos del input
+    
+    ToDoList.push(datos.children[0].value, datos.children[1].value)
+}
+
+function UpdateGrid()
+{
+
+}
+
+function ChangeStatus()
+{}
